7898175537cb4e2859c87b8bc99a5842fe920846
Alcides Queiroz <alcidesqueiroz@gmail.com>
2018-05-01 22:04:52 -0300

    Array extending logic

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 'use strict';
 
+const ex = {};
 
-module.exports = {};
+const exSymbol = Symbol('ex');
+
+function extendArray() {
+  Object.defineProperty(Array.prototype, 'ex', {
+    get() {
+      if (this[exSymbol]) return this[exSymbol];
+
+      const abe = {};
+
+      Object.entries(ex).forEach((entry) => {
+        abe[entry[0]] = (...args) => entry[1](this, ...args);
+      });
+
+      this[exSymbol] = abe;
+      return abe;
+    }
+  });
+}
+
+module.exports = { ...ex, extendArray };
